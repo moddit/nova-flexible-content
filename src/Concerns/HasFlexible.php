b110feb5c17fa0ec829423d04e2fd2cb1cda1c98
Wessel Perik <wessel@perik.me>
2022-11-15 15:58:28 +0100

    🐛 Added fix for casting/resolving layouts
    See whitecube/nova-flexible-content#409

--- src/Concerns/HasFlexible.php
+++ src/Concerns/HasFlexible.php
@@ -5,7 +5,6 @@ namespace Whitecube\NovaFlexibleContent\Concerns;
 use Whitecube\NovaFlexibleContent\Layouts\Layout;
 use Whitecube\NovaFlexibleContent\Layouts\Collection;
 use Illuminate\Support\Collection as BaseCollection;
-use Laravel\Nova\NovaServiceProvider;
 use Whitecube\NovaFlexibleContent\Value\FlexibleCast;
 
 trait HasFlexible {
@@ -32,22 +31,6 @@ trait HasFlexible {
      * @return \Whitecube\NovaFlexibleContent\Layouts\Collection
      */
     public function cast($value, $layoutMapping = [])
-    {
-        if(app()->getProvider(NovaServiceProvider::class) && !app()->runningInConsole() && !app()->environment('testing')) {
-            return $value;
-        }
-
-        return $this->toFlexible($value ?: null, $layoutMapping);
-    }
-
-    /**
-     * Parse a Flexible Content from value
-     *
-     * @param mixed $value
-     * @param array $layoutMapping
-     * @return \Whitecube\NovaFlexibleContent\Layouts\Collection
-     */
-    public function toFlexible($value, $layoutMapping = [])
     {
         $flexible = $this->getFlexibleArrayFromValue($value);
 
--- src/Value/Resolver.php
+++ src/Value/Resolver.php
@@ -3,6 +3,8 @@
 namespace Whitecube\NovaFlexibleContent\Value;
 
 use Illuminate\Support\Collection;
+use Whitecube\NovaFlexibleContent\Layouts\Collection as LayoutsCollection;
+use Whitecube\NovaFlexibleContent\Layouts\Layout;
 
 class Resolver implements ResolverInterface
 {
@@ -39,11 +41,14 @@ class Resolver implements ResolverInterface
         $value = $this->extractValueFromResource($resource, $attribute);
 
         return collect($value)->map(function($item) use ($layouts) {
-            $layout = $layouts->find($item->layout);
+            $layout = $layouts->find($item instanceof Layout ? $item->name() : $item->layout);
 
             if(!$layout) return;
 
-            return $layout->duplicateAndHydrate($item->key, (array) $item->attributes);
+            $key = $item instanceof Layout ? $item->key() : $item->key;
+            $attributes = $item instanceof Layout ? $item->getAttributes() : (array) $item->attributes;
+
+            return $layout->duplicateAndHydrate($key, $attributes);
         })->filter()->values();
     }
 
@@ -58,7 +63,9 @@ class Resolver implements ResolverInterface
     {
         $value = data_get($resource, str_replace('->', '.', $attribute)) ?? [];
 
-        if($value instanceof Collection) {
+        if ($value instanceof LayoutsCollection) {
+            $value = $value->all();
+        } elseif ($value instanceof Collection) {
             $value = $value->toArray();
         } else if (is_string($value)) {
             $value = json_decode($value) ?? [];
